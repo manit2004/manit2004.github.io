0949bb23b9a69d3fda3e282fa6935af8659cc18d
manit2004 <manit.roy@research.iiit.ac.in>
2024-02-06 23:50:49 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openBlog(blogId) {
+    if (blogId === 'blog1') {
+        window.open('https://example.com/blog1', '_blank');
+    } else if (blogId === 'blog2') {
+        window.open('https://example.com/blog2', '_blank');
+    }
+}
